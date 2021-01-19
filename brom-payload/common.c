c81b18943c339ddffe1d043a266ecd6c29fd820b
Vladimir <vladimir.ghost719@yandex.ru>
2021-01-20 01:12:25 +0300

    brom-payload: fix send_data and recv_data

--- brom-payload/common.c
+++ brom-payload/common.c
@@ -10,9 +10,9 @@ void (*orig_ptr_recv)();
 int (*send_dword)() = (void*)0xBCD3;
 int (*recv_dword)() = (void*)0xBC9F;
 // addr, sz
-int (*send_data)() = (void*)0xBD15;
+int (*send_data)() = (void*)0xBDA3;
 // addr, sz, flags (=0)
-int (*recv_data)() = (void*)0xBDA3;
+int (*recv_data)() = (void*)0xBD15;
 
 void low_uart_put(int ch) {
     volatile uint32_t *uart_reg0 = (volatile uint32_t*)0x11002014;
